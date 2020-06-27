9aca269277d59f16eef6e9ed77e159ca74cf6a33
Luke Parker <lukemparker@outlook.com>
2020-06-27 19:51:50 +1000

    Hook to events when bot reconnects

--- bot.js
+++ bot.js
@@ -10,53 +10,61 @@ var bot = mineflayer.createBot({
   username: config.userEmail,
   password: config.userPassword,
   version: false, // Auto-version
-  viewDistance: config.renderDistance
+  viewDistance: config.renderDistance,
 });
 
-bot.once('spawn', () => {
-    bot.on("chat", function (username, message) {
-        if (username === bot.username || username !== config.owner) return;
-      
-        if (message.startsWith(config.commandPrefix)) {
-          fullCommand = message.split(config.commandPrefix, 2)[1]
-          commandParts = fullCommand.split(" ", 2);
-          command = commandParts[0]
-          args = commandParts[1]
-      
-          switch (command) {
-            case "tpahere":
-              bot.chat(`/tpa ${username}`)  
-              break;
-            case "look":
-              target = bot.players[args].entity;
-              if (target === null) {
-                  console.log(`Could not find ${username}`)
-                  return
-              }
-              bot.lookAt(target.position)
-              break;
-            case "stop":
-                bot.navigate.stop()
-                console.log("Stopped all actions")
-                break;
+function handleChat(username, message) {
+    if (username === bot.username || username !== config.owner) return;
+  
+    if (message.startsWith(config.commandPrefix)) {
+      fullCommand = message.split(config.commandPrefix, 2)[1];
+      commandParts = fullCommand.split(" ", 2);
+      command = commandParts[0];
+      args = commandParts[1];
+  
+      switch (command) {
+        case "tpahere":
+          bot.chat(`/tpa ${username}`);
+          break;
+        case "look":
+          target = bot.players[args].entity;
+          if (target === null) {
+            console.log(`Could not find ${username}`);
+            return;
           }
-        }
-      });
-      
-      bot.on("end", () => {
-        console.log("Disconnected from server, waiting to reconnect")
-        setTimeout(function() {
-            bot = mineflayer.createBot({
-                host: config.host,
-                port: config.port,
-                username: config.userEmail,
-                password: config.userPassword,
-                version: false, // Auto-version
-                viewDistance: config.renderDistance
-              });
-        }, 4 * 60 * 1000);
-      });
+          bot.lookAt(target.position);
+          break;
+        case "stop":
+          bot.navigate.stop();
+          console.log("Stopped all actions");
+          break;
+      }
+    }
+  }
 
-      bot.on("error", (err) => console.log(err));
-});
+function handleError(){
+    console.log(err)
+}
 
+function handleEnd(){
+    console.log("Disconnected from server, waiting to reconnect");
+    setTimeout(function () {
+      bot = mineflayer.createBot({
+        host: config.host,
+        port: config.port,
+        username: config.userEmail,
+        password: config.userPassword,
+        version: false, // Auto-version
+        viewDistance: config.renderDistance,
+      });
+      hookEvents()
+    }, 4 * 60 * 1000);
+}
+
+function hookEvents(){
+    bot.on("chat", handleChat);
+    bot.on("end", handleEnd);
+    bot.on("error", handleError);
+}
+
+hookEvents()
