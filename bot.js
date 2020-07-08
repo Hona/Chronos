e9a97a51275666ab2e559e12ab159bb215cdbf20
Luke Parker <lukemparker@outlook.com>
2020-07-08 15:41:47 +1000

    Format, and fix bot not splitting on first space only

--- bot.js
+++ bot.js
@@ -14,69 +14,70 @@ var bot = mineflayer.createBot({
 });
 
 function handleChat(username, message) {
-    if (username === bot.username || username !== config.owner) return;
-  
-    if (message.startsWith(config.commandPrefix)) {
-      fullCommand = message.split(config.commandPrefix, 2)[1];
-      commandParts = fullCommand.split(" ", 2);
-      command = commandParts[0];
-      args = commandParts[1];
-  
-      switch (command) {
-        case "tpahere":
-          bot.chat(`/tpa ${username}`);
-          break;
-        case "look":
-          target = bot.players[args].entity;
-          if (target === null) {
-            console.log(`Could not find ${username}`);
-            return;
-          }
-          bot.lookAt(target.position);
-          break;
-        case "stop":
-          bot.navigate.stop();
-          console.log("Stopped all actions");
-          break;
-        case "ping":
-          bot.chat("Pong!");
-          break;
-        case "exec":
-          bot.chat(args);
-          break;
-        case "sethome":
-          bot.chat("/sethome");
-          break;
-        case "home":
-          bot.chat("/home")
-          break;
-      }
+  console.log(username + " " + message);
+  if (username === bot.username || username !== config.owner) return;
+
+  if (message.startsWith(config.commandPrefix)) {
+    fullCommand = message.split(config.commandPrefix, 2)[1];
+    commandParts = fullCommand.split(/ (.+)/);
+    command = commandParts[0];
+    args = commandParts[1];
+
+    switch (command) {
+      case "tpahere":
+        bot.chat(`/tpa ${username}`);
+        break;
+      case "look":
+        target = bot.players[args].entity;
+        if (target === null) {
+          console.log(`Could not find ${username}`);
+          return;
+        }
+        bot.lookAt(target.position);
+        break;
+      case "stop":
+        bot.navigate.stop();
+        console.log("Stopped all actions");
+        break;
+      case "ping":
+        bot.chat("Pong!");
+        break;
+      case "exec":
+        bot.chat(args);
+        break;
+      case "sethome":
+        bot.chat("/sethome home");
+        break;
+      case "home":
+        bot.chat("/home home");
+        break;
     }
   }
-
-function handleError(){
-    console.log(err)
 }
 
-function handleEnd(){
-    console.log("Disconnected from server, waiting to reconnect");
-    setTimeout(function () {
-      bot = mineflayer.createBot({
-        host: config.host,
-        port: config.port,
-        username: config.userEmail,
-        password: config.userPassword,
-        version: false, // Auto-version
-        viewDistance: config.renderDistance,
-      });
-      hookEvents()
-    }, 4 * 60 * 1000);
+function handleError() {
+  console.log(err);
 }
 
-function hookEvents(){
-    bot.on("chat", handleChat);
-    bot.on("end", handleEnd);
-    bot.on("error", handleError);
+function handleEnd() {
+  console.log("Disconnected from server, waiting to reconnect");
+  setTimeout(function () {
+    bot = mineflayer.createBot({
+      host: config.host,
+      port: config.port,
+      username: config.userEmail,
+      password: config.userPassword,
+      version: false, // Auto-version
+      viewDistance: config.renderDistance,
+    });
+    hookEvents();
+  }, 4 * 60 * 1000);
 }
 
-hookEvents()
+function hookEvents() {
+  bot.on("chat", handleChat);
+  bot.on("end", handleEnd);
+  bot.on("error", handleError);
+}
+
+hookEvents();
